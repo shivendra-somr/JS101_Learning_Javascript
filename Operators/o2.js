8786fa906f4fd5e91e79852a97e59c1cb73722ca
shivendra-somr <shivendra.somr@gmail.com>
2023-01-31 14:35:05 +0530

    operators_problem

---
+++ Operators/o2.js
@@ -0,0 +1,5 @@
+let a = 1;
+multiplyBy50(a)
+function multiplyBy50(number) {
+    console.log(number*50)
+}
---
+++ Operators/o6.js
@@ -0,0 +1,5 @@
+function squareAndSum(one, two, three) {
+    sum = (one**2 + two**2 + three**2);
+    console.log(sum);
+  }
+squareAndSum(1,2,3);  
