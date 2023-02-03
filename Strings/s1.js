282bb5ec729ac69bbb3c0b249ab2c1a345ddd06b
shivendra-somr <shivendra.somr@gmail.com>
2023-02-03 20:45:18 +0530

    String_problems

---
+++ Strings/s1.js
@@ -0,0 +1,2 @@
+let arr = ["World","Javascript","Ftweb","masai"];
+console.log(arr.length);
---
+++ Strings/s2.js
@@ -0,0 +1,6 @@
+let arr = ["World","Javascript","Ftweb","masai","school"];
+let k = 0;
+for (let i = 0; i<arr.length;i++){
+  k += arr[i].length;
+}
+console.log(k);
